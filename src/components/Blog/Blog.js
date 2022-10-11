5159de159250f2b4990093e800aa2a995f3d0d83
Araf <arafathossain.baiust@gmail.com>
2022-10-11 21:13:10 +0600

    blog done

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,19 +4,19 @@ import './Blog.css'
 const Blog = () => {
     return (
         <div className='conatiner'>
-            <div>
-                <h3>Question: What is the purpose of react router</h3>
-                <p>Answer: React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</p>
+            <div className='bottom'>
+                <h4><span className='que'>Question:</span> What is the purpose of react router?</h4>
+                <p><span className='ans'>Answer:</span> React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</p>
             </div>
 
-            <div>
-                <h3></h3>
-                <p></p>
+            <div className='bottom'>
+                <h4><span className='que'>Question:</span> How does context api works?</h4>
+                <p><span className='ans'>Answer:</span> The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
             </div>
 
-            <div>
-                <h3></h3>
-                <p></p>
+            <div className='bottom'>
+                <h4><span className='que'>Question:</span> What is useRef hook in react?</h4>
+                <p><span className='ans'>Answer:</span> The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.</p>
             </div>
         </div>
     );
